8dda74cddf3657a2a12599cf6859df8d1c5b9b31
piotr.w.dabrowski2@gmail.com <Piotr Dbrowski>
2024-02-12 15:15:09 +0100

    admin usage first time

--- meeting_planner/meetings/models.py
+++ meeting_planner/meetings/models.py
@@ -17,7 +17,6 @@ class Meeting(models.Model):
     date = models.DateField()
     start_time = models.TimeField(default=time(9))
     duration = models.IntegerField(default=1)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.title} at {self.start_time} on {self.date}"
+        return f"{self.title} at {self.start_time} on {self.date} and will take about {self.duration} hour"
