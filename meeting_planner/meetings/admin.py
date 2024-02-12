fe653e86bf2f320e3e062e358d9bfd53888f8114
piotr.w.dabrowski2@gmail.com <Piotr Dbrowski>
2024-02-12 15:56:40 +0100

    Building & Room added, Foreign Key used

--- meeting_planner/meetings/admin.py
+++ meeting_planner/meetings/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Meeting, Room
+from .models import Meeting, Room, Building
 
 
+admin.site.register(Building)
 admin.site.register(Meeting)
 admin.site.register(Room)
 
--- meeting_planner/meetings/models.py
+++ meeting_planner/meetings/models.py
@@ -1,22 +1,29 @@
-from datetime import time
-
 from django.db import models
 
 
-class Room(models.Model):
-    name = models.CharField(max_length=50)
-    floor = models.IntegerField()
-    room_number = models.IntegerField()
+class Building(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
 
     def __str__(self):
-        return f"{self.name}: room {self.room_number} on floor {self.floor}"
+        return self.name
+
+
+class Room(models.Model):
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
+    number = models.CharField(max_length=10)
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.building.name} Room {self.number}'
 
 
 class Meeting(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
-    start_time = models.TimeField(default=time(9))
-    duration = models.IntegerField(default=1)
+    start_time = models.TimeField()
+    duration = models.IntegerField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.title} at {self.start_time} on {self.date} and will take about {self.duration} hour"
+        return self.title
